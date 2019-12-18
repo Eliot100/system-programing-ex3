aa45b8e8b1ecd321cc7e6c87783f2521da0d0798
Eliot100 <46245107+Eliot100@users.noreply.github.com>
2019-12-19 01:40:26 +0200

    final

--- isort.c
+++ isort.c
@@ -3,41 +3,38 @@
 //input :The function will get an array pointer and a number of moves
 //output:Returns the exponent that all numbers move one forward
 void shift_element(int *arr,int i){
-    do
-    { 	
+    while (i>0){ 	
       *(arr+i+1)=*(arr+i);
 	  i--;
-    }while (i>=0);
+    }
 }
 //input  :The puncture gets array
 //output :sort they array
 
 void insertion_sort(int *arr,int len){
-    int i=1,j=0;
-    for (;i<len;i++)
-    {
-        j=i-1;
-       if(*(arr+j)>*(arr+i)){
-           while(*(arr+j)>*(arr+i)){
-           j--;
-            }
-        int temp=*(arr+i);
-        shift_element((arr+j),(i-j-1));
-        *(arr+j+1)=temp;
+    for (int i=1, j=0;i<len;i++) {
+		j=i-1;
+		if(*(arr+j)>*(arr+i)){
+			while(*(arr+j)>*(arr+i)){
+				j--;
+			}
+			int temp=*(arr+i);
+			shift_element((arr+j),(i-j-1));
+			*(arr+j+1)=temp;
         }
     }  
 }
 int main(){
     int arr[size]={0};
-    int i=0;
-    for(;i<(size); i++)   //data reception 
-        {scanf("%d",&arr[i]);}
-    insertion_sort(arr,size); 
-    i=0;   // sort arr
-    for(; i< size; i++){ //Data Display ofter sort
-		if(i==0)printf("%d",arr[i]);
-		else printf(",%d",arr[i]);
+    for(int i=0; i<(size); i++){  //data reception 
+        scanf("%d",&arr[i]);
 	}
-		printf("\n");
+    insertion_sort(arr,size); // sort arr
+    for(int i=0; i< size; i++){ //Data Display after sort
+		if(i==0)
+			printf("%d",arr[i]);
+		else {printf(",%d",arr[i]);}
+	}
+	printf("\n");
     return 0;
 }
